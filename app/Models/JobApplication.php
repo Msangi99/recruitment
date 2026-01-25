e2271295240b9d5f0ff4ac620ce542b7a27543e2
msangi99 <doniaparoma99@gmail.com>
2026-01-25 23:47:21 +0300

    oi

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -14,6 +14,7 @@ class JobApplication extends Model
         'job_id',
         'candidate_id',
         'cover_letter',
+        'cv_path',
         'video_path',
         'status',
         'reviewed_at',
