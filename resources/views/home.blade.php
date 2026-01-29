4b58284584937ef0601c4e5cb4121d4a20bbf805
msangi99 <doniaparoma99@gmail.com>
2026-01-29 19:35:07 +0300

    dd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name') }} - Professional Overseas Recruitment Platform</title>
+    <title>{{ config('app.name') }}</title>
     <link rel="icon" type="image/png" href="{{ asset('logo-removed-background.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
